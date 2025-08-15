e7ebe4a29767680b6296be37ad80af162d49233a
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-08-15 15:06:14 +0530

    drizzle neon setup

---
+++ drizzle/0000_true_green_goblin.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "files" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"path" text NOT NULL,
+	"size" integer NOT NULL,
+	"type" text NOT NULL,
+	"fileUrl" text NOT NULL,
+	"userId" text NOT NULL,
+	"parentId" uuid,
+	"isFolder" boolean DEFAULT false NOT NULL,
+	"isStared" boolean DEFAULT false NOT NULL,
+	"isTrashed" boolean DEFAULT false NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
